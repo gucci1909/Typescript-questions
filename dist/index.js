f7057dd24b07c05a178371f5473257eb2a5b402c
gucci1909 <umangarora0134@gmail.com>
2023-02-09 01:09:27 +0530

    typescript questions

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const b = 290;
-console.log(b);
+const b = 3;
+const a = "umang";
+const c = undefined;
+const d = null;
+const e = true;
+// console.log(b,a,c,d,e);
+const f1 = [1, 2, 3, 4, 5];
+const f2 = [2, 3, 4, 5, 6];
+const g1 = ["umang", "bunny"];
+const g2 = ["umang", 3, "bunny", 5];
+const h1 = [[0, "umang"], [3, "bunny"]];
+const i = [
+    { id: 1, name: "umang", bol: true, age: { value: 1 } },
+    { id: 2, name: "prashant", bol: false },
+    { id: 3, name: "deepak", bol: true }
+];
+var a3 = i[0].age?.value;
+console.log(a3);
+// console.log(f1,f2,g1,g2,h1,i);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
-const b:number = 290;
-console.log(b);
+const b:number|null= 3;
+const a:string = "umang";
+const c:undefined = undefined;
+const d:null = null;
+const e:boolean = true;
+console.log(b,a,c,d,e);
+
+const f1:number[] = [1,2,3,4,5];
+const f2:Array<number> = [2,3,4,5,6];
+const g1:string[]= ["umang","bunny"];
+const g2:Array<string|number> = ["umang",3,"bunny",5];
+const h1:Array<Array<number| string>> =[[0,"umang"],[3,"bunny"]];
+
+const i:Array<{
+    id:number,
+    name:string,
+    bol:boolean,
+    age?:{
+        value:number
+    }
+}> = [
+    {id:1,name:"umang",bol:true,age:{value:1}},
+    {id:2,name:"prashant",bol:false},
+    {id:3,name:"deepak",bol:true}
+]
+
+var a3:number|undefined = i[0].age?.value;
+console.log(a3);
+
+
+console.log(f1,f2,g1,g2,h1,i);
