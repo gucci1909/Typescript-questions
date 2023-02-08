d484797f6aa114f1dece57ab8138eecd879a46c5
gucci1909 <umangarora0134@gmail.com>
2023-02-09 01:27:53 +0530

    typescript questions

--- dist/index.js
+++ dist/index.js
@@ -5,18 +5,42 @@ const a = "umang";
 const c = undefined;
 const d = null;
 const e = true;
-// console.log(b,a,c,d,e);
+console.log(b, a, c, d, e);
 const f1 = [1, 2, 3, 4, 5];
 const f2 = [2, 3, 4, 5, 6];
 const g1 = ["umang", "bunny"];
 const g2 = ["umang", 3, "bunny", 5];
-const h1 = [[0, "umang"], [3, "bunny"]];
+const h1 = [
+    [0, "umang"],
+    [3, "bunny"],
+];
 const i = [
     { id: 1, name: "umang", bol: true, age: { value: 1 } },
     { id: 2, name: "prashant", bol: false },
-    { id: 3, name: "deepak", bol: true }
+    { id: 3, name: "deepak", bol: true },
 ];
 var a3 = i[0].age?.value;
 console.log(a3);
-// console.log(f1,f2,g1,g2,h1,i);
+console.log(f1, f2, g1, g2, h1, i);
+const names = ["INDIA", "PAKISTAN"];
+console.log(names);
+const customers = [
+    {
+        id: 1,
+        name: "umang",
+        gender: "MALE",
+    },
+    {
+        id: 1,
+        name: "umang",
+        gender: "MALE",
+    },
+];
+//tuple
+const making = [1, "umang"];
+const making2 = [
+    [1, "umang"],
+    [2, "vansh"],
+];
+console.log(customers);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,65 @@
-const b:number|null= 3;
-const a:string = "umang";
-const c:undefined = undefined;
-const d:null = null;
-const e:boolean = true;
-console.log(b,a,c,d,e);
+const b: number | null = 3;
+const a: string = "umang";
+const c: undefined = undefined;
+const d: null = null;
+const e: boolean = true;
+console.log(b, a, c, d, e);
 
-const f1:number[] = [1,2,3,4,5];
-const f2:Array<number> = [2,3,4,5,6];
-const g1:string[]= ["umang","bunny"];
-const g2:Array<string|number> = ["umang",3,"bunny",5];
-const h1:Array<Array<number| string>> =[[0,"umang"],[3,"bunny"]];
+const f1: number[] = [1, 2, 3, 4, 5];
+const f2: Array<number> = [2, 3, 4, 5, 6];
+const g1: string[] = ["umang", "bunny"];
+const g2: Array<string | number> = ["umang", 3, "bunny", 5];
+const h1: Array<Array<number | string>> = [
+  [0, "umang"],
+  [3, "bunny"],
+];
 
-const i:Array<{
-    id:number,
-    name:string,
-    bol:boolean,
-    age?:{
-        value:number
-    }
+const i: Array<{
+  id: number;
+  name: string;
+  bol: boolean;
+  age?: {
+    value: number;
+  };
 }> = [
-    {id:1,name:"umang",bol:true,age:{value:1}},
-    {id:2,name:"prashant",bol:false},
-    {id:3,name:"deepak",bol:true}
-]
+  { id: 1, name: "umang", bol: true, age: { value: 1 } },
+  { id: 2, name: "prashant", bol: false },
+  { id: 3, name: "deepak", bol: true },
+];
 
-var a3:number|undefined = i[0].age?.value;
+var a3: number | undefined = i[0].age?.value;
 console.log(a3);
 
+console.log(f1, f2, g1, g2, h1, i);
 
-console.log(f1,f2,g1,g2,h1,i);
+type Country = "INDIA" | "PAKISTAN" | "CHINA";
+const names: Array<Country> = ["INDIA", "PAKISTAN"];
+console.log(names);
+
+type GENDER = "MALE" | "FEMALE";
+type DETAILS = {
+  id: number;
+  name: string;
+  gender: GENDER;
+};
+const customers: Array<DETAILS> = [
+  {
+    id: 1,
+    name: "umang",
+    gender: "MALE",
+  },
+  {
+    id: 1,
+    name: "umang",
+    gender: "MALE",
+  },
+];
+
+//tuple
+const making: [number, string] = [1, "umang"];
+const making2: Array<[number, string]> = [
+  [1, "umang"],
+  [2, "vansh"],
+];
+
+console.log(customers);
